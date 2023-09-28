ff89427973da31cf21c67248eb6ac1361ca6aee7
Arun61045 <arunsharma61045@gmail.com>
2023-09-28 15:25:39 +0530

    End of section 10

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -24,13 +24,15 @@ export class ShopService {
     params = params.append('pageSize',shopParams.pageSize);
     if (shopParams.search) params = params.append('search',shopParams.search);
 
-
-
-
-
-    
-    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'Products', {params});
+ return this.http.get<Pagination<Product[]>>(this.baseUrl + 'Products', {params});
   }
+
+  getProduct(id:number){
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+
+  }
+
+
    getBrands(){
     return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
    }
